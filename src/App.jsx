a01ce442549e0dee956e8268024f8b711e905d25
Sowmitra Guha <sowmmochy@gmail.com>
2025-06-14 03:37:06 +0600

    project started, tailwind and daiysui installed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      
+      
+    </>
+  )
+}
+
+export default App
