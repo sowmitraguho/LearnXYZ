aec455d82368e019ee2656d7816efdf137e739fd
Sowmitra Guha <sowmmochy@gmail.com>
2025-06-17 17:31:33 +0600

    booking and confirmattion modal designed and connected to server

---
+++ src/Components/BookingModal/BookingModals.jsx
@@ -0,0 +1,109 @@
+import axios from 'axios';
+import React from 'react';
+import Swal from 'sweetalert2';
+
+const BookingModals = ({ serviceData, handleCloseModal }) => {
+    //console.log(serviceData);
+
+    const handleconfirmBooking = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const data = new FormData(form);
+        const confirmedData = Object.fromEntries(data.entries());
+        console.log('form submitted', confirmedData);
+        axios.post('https://learnxyz-server.onrender.com/bookedServices', confirmedData)
+            .then(res => {
+                console.log('after adding in mogodb', res);
+                
+                Swal.fire({
+                    title: "Service Published Sucessfully!",
+                    icon: "success",
+                    timer: 2000,
+                    draggable: true
+                });
+                handleCloseModal();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        
+    }
+
+
+
+
+    return (
+        <div>
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-transparent bg-opacity-50">
+                <div className="bg-gray-200 m-12 w-11/12 p-6 rounded shadow-md max-w-full max-h-[90vh] overflow-y-auto">
+                    <h2 className="text-2xl font-semibold mb-4 text-center">Admission Confirmation</h2>
+                    <form onSubmit={handleconfirmBooking}>
+                        <div className="grid gap-6 mb-6 md:grid-cols-2">
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service ID</label>
+                                <input name='serviceId' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={serviceData.serviceId} placeholder="Service ID" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service name</label>
+                                <input name='serviceName' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={serviceData.serviceName} placeholder="Service Name" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service Provider name</label>
+                                <input name='providerName' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={serviceData.providerName} placeholder="Service Provider Name" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service Provider Email</label>
+                                <input name='providerEmail' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={serviceData.providerEmail} placeholder="Service Provider Email" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service Area</label>
+                                <input name='serviceArea' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={serviceData.serviceArea} placeholder="Service Field" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Service Image URL</label>
+                                <input name='imageUrl' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Service Image URL" defaultValue={serviceData.serviceImage} />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                                <input name='price' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$000" defaultValue={serviceData.price} />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Student name</label>
+                                <input name='studentName' type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={serviceData.studentName} placeholder="Student Name" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Student Email</label>
+                                <input name='studentEmail' type="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={serviceData.studentEmail} placeholder="Student Email" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Course Starting Date</label>
+                                <input name='date' type="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            </div>
+                            
+                        </div>
+                        <div className="mb-6">
+                                <label htmlFor="instructions" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Instruction for the course</label>
+                                <textarea name='instructions' id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Any Special Instruction for the course" />
+                            </div>
+                        <div className="flex justify-center gap-4">
+                            <button type='submit'
+                                className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded"
+                            >
+                                Confirm
+                            </button>
+                            <button
+                                onClick={handleCloseModal}
+                                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookingModals;
--- src/Pages/SeviceDetails/ServiceDetails.jsx
+++ src/Pages/SeviceDetails/ServiceDetails.jsx
@@ -5,55 +5,78 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 import { FaDiscord, FaFacebook, FaWhatsapp } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
+import BookingModals from '../../Components/BookingModal/BookingModals';
 
 const ServiceDetails = () => {
     const { loggedInUser } = use(AuthContext);
     const serviceData = useLoaderData().data;
     const [booked, setBooked] = useState(false);
-    const handleEnrollment = () => {
-        console.log('Enrolled by', loggedInUser);
-        const bookedServiceData = {
-            studentName: loggedInUser.displayName,
-            studenEmail: loggedInUser.email,
-            studentId: loggedInUser.uid,
-            serviceName: serviceData.serviceName,
-            serviceId: serviceData._id,
-            serviceImage: serviceData.imageurl
-        }
-        console.log(bookedServiceData);
-        axios.post('https://learnxyz-server.onrender.com/bookedServices', bookedServiceData)
-            .then(res => {
-                console.log('after adding in mogodb', res);
-                setBooked(true);
-                Swal.fire({
-                    title: "Service Published Sucessfully!",
-                    icon: "success",
-                    timer: 2000,
-                    draggable: true
-                });
+    const [showModal, setShowModal] = useState(false);
+    const bookedServiceData = {
+        studentName: loggedInUser.displayName,
+        studentEmail: loggedInUser.email,
+        studentId: loggedInUser.uid,
+        serviceName: serviceData.serviceName,
+        serviceArea: serviceData.serviceArea,
+        serviceId: serviceData._id,
+        serviceImage: serviceData.imageUrl,
+        price: serviceData.price,
+        providerName: serviceData.providerName,
+        providerEmail: serviceData.providerEmail,
+    }
 
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+
+    
+
+    const handleOpenModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
+
+
+
+
+
+    const handleEnrollment = async () => {
+        console.log('Enrolled by', loggedInUser);
+
+        console.log(bookedServiceData);
+        handleOpenModal();
+        setBooked(true);
+        // axios.post('https://learnxyz-server.onrender.com/bookedServices', bookedServiceData)
+        //     .then(res => {
+        //         console.log('after adding in mogodb', res);
+        //         setBooked(true);
+        //         Swal.fire({
+        //             title: "Service Published Sucessfully!",
+        //             icon: "success",
+        //             timer: 2000,
+        //             draggable: true
+        //         });
+
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     });
     }
     return (
         <div className='p-24'>
 
             <section className='flex gap-6 items-center justify-between shadow-xl border border-gray-200 dark:border-gray-900 p-12  bg-white dark:bg-gray-900'>
-                <div className="left-section">
-                    <h1 className="text-5xl font-bold dark:text-white">{serviceData.serviceName}</h1>
+                <div className="left-section w-full max-w-full">
+                    <h1 className="text-4xl font-bold dark:text-white">{serviceData.serviceName}</h1>
                     <h2 className="text-2xl my-2 flex items-center gap-2">Course Fee: <span className='text-blue-600 font-extrabold text-4xl'>${serviceData.price}</span> </h2>
                     <p className="text-xl mb-2">Course Category: <span className='font-semibold'>{serviceData.serviceArea}</span> </p>
                     <p className="text-xl mb-2">Course Duration: <span className='text-purple-600 font-semibold'>125 hours</span></p>
                     <p className='mb-2 text-lg font-semibold' >Ratings: <span className='text-green-600 dark:text-yellow-200'>4.5</span> </p>
                     <button onClick={handleEnrollment} disabled={booked} type="button" className='w-50 text-white font-bold bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 rounded-lg px-5 py-2.5 text-center me-2 mb-2 disabled:cursor-not-allowed'>Enroll Now</button>
                 </div>
-                <div className="right-section">
-                    <img className='rounded-lg max-w-120 ' src={serviceData.imageUrl} alt="" />
+                <div className="right-section w-full">
+                    <img className='rounded-lg max-w-full ' src={serviceData.imageUrl} alt="" />
                 </div>
             </section>
-
+            <section>
+                {showModal && <BookingModals serviceData={bookedServiceData} handleCloseModal={handleCloseModal} />}
+            </section>
             <section className='my-6 bg-white p-8  text-xl shadow-xl border border-gray-200 dark:border-gray-900 dark:bg-gray-900'>
                 <h2 className="text-3xl font-bold">Course Details</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
